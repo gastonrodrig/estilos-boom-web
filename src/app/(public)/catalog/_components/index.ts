c16e7b0e8f71712ce13582bfc93ea665a34a9ec1
Gaston Rodriguez <gaston.rodriguez@icloud.com>
2026-02-05 11:37:12 -0500

    Borrara importaciones inexistentes nuevamente

--- src/app/(public)/catalog/_components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./color-swatch";
-export * from "./price-tags";
-export * from "./stars";
-export * from "./catalog-toolbar";
-export * from "./product-grid";
-export * from "./product-card";
